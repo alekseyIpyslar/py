4fe85466f387f52940fd8c574d0845c318a6a9fa
Alexei Pislari <alexei.pislari@iongroup.com>
2024-09-10 16:36:05 +0300

    feat: initial commit

---
+++ poop.py
@@ -0,0 +1,9 @@
+from car import Car
+
+car1 = Car("Mustang", 2024, "black", True)
+car2 = Car("Corvette", 2025, "red", False)
+car3 = Car("Charger", 2026, "blue", False)
+
+car3.drive()
+car3.stop()
+car3.describe()
